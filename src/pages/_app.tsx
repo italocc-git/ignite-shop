4f10f78e2df1c04aa4f6dc7c0fc54908ccaf00a9
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-11-10 16:04:52 -0300

    feature: estrutura visual

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
+import { globalStyles } from "../styles/global"
+
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
